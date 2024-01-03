b608c22269ad12ea2af54d481fac90953ab18a07
ArHei <47974374+ArHei@users.noreply.github.com>
2024-01-03 09:19:41 +0000

    Changes to be committed: 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "codespaces-react-qrcode-scanner",
   plugins: [react()],
   test: {
     globals: true,
